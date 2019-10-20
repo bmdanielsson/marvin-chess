316891f265cf4bf593689a3120187bde718195ec
Martin Danielsson <mdanielsson75@gmail.com>
2019-10-20 12:38:10 +0200

    genevalparams.py: Change to update existing file
    Update script so that it updates an existing file rather than
    creating a new one.

--- scripts/genevalparams.py
+++ scripts/genevalparams.py
@@ -1,69 +1,112 @@
 #!/usr/bin/env python3
+import os
 import sys
-import chess
-import chess.pgn
+import tempfile
 
-def print_gpl_header(output):
-    output.write('/*\n');
-    output.write(' * Marvin - an UCI/XBoard compatible chess engine\n')
-    output.write(' * Copyright (C) 2015 Martin Danielsson\n')
-    output.write(' *\n')
-    output.write(' * This program is free software: you can redistribute it and/or modify\n')
-    output.write(' * it under the terms of the GNU General Public License as published by\n')
-    output.write(' * the Free Software Foundation, either version 3 of the License, or\n')
-    output.write(' * (at your option) any later version.\n')
-    output.write(' *\n')
-    output.write(' * This program is distributed in the hope that it will be useful,\n')
-    output.write(' * but WITHOUT ANY WARRANTY; without even the implied warranty of\n')
-    output.write(' * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n')
-    output.write(' * GNU General Public License for more details.\n')
-    output.write(' *\n')
-    output.write(' * You should have received a copy of the GNU General Public License\n')
-    output.write(' * along with this program.  If not, see <http://www.gnu.org/licenses/>.\n')
-    output.write(' */\n');
-
-def print_include_directives(output):
-    output.write('#include "evalparams.h"\n')
-
-def print_psq_table(output, name, line):
-    parts = line.split('{')
-    values = parts[1][:-1].split(', ')
-    str = 'int %s[NSQUARES] = {\n    ' % (name.upper())
-    count = 0;
-    for value in values:
-        str = str + value + ', '
-        count = count + 1;
-        if (count == 8):
-            str = str[:-1]
-            str = str + '\n    '
+def write_array_variable(outputfile, name, value):
+    values = value[1:-1].split(',')
+    str = f'int {name} = {{\n    '
+    count = 0
+    for v in values:
+        v = v.lstrip().rstrip()
+        str += f'{v}, '
+        count += 1
+        if count == 8:
+            str = str[:-1] + '\n    '
             count = 0
-    str = str[:-6]
-    str = str + '\n};\n'
-    output.write(str)
+    if count == 0:
+        str = str[:-6]
+    else:
+        str = str[:-2]
+    str += '\n};\n'
 
-def print_param(output, name, value):
-    str = 'int %s = %s;\n' % (name.upper(), value)
-    output.write(str)
+    outputfile.write(str)
 
-if len(sys.argv) != 2:
+def write_variable(outputfile, name, value):
+    if not '[' in name:
+        outputfile.write(f'int {name} = {value};\n')
+    else:
+        write_array_variable(outputfile, name, value)
+
+def parse_array_value(paramfile, value):
+    array_value = value;
+    while not ';' in array_value:
+        line = paramfile.readline()
+        line = line.rstrip().lstrip()
+        array_value += line
+
+    return array_value
+
+def parse_variable(paramfile, line):
+    line = line[line.find(' ')+1:]
+
+    name = line[:line.find(' ')]
+    value = line[line.find('=')+1:]
+    value = value.lstrip();
+
+    if '[' in name:
+        if not ';' in value:
+            value = parse_array_value(paramfile, value)
+
+    # Skip semicolon at the end
+    value = value[:-1]
+
+    return (name, value)
+
+def parse_param(line):
+    return (line[:line.find(' ')], line[line.find(' ')+1:])
+
+def read_input_file(inputpath):
+    tuning_dict = {}
+    with open(inputpath) as inputfile:
+        for line in inputfile:
+            line = line.rstrip()
+            if len(line) == 0:
+                continue
+            if line[0] == '#':
+                continue
+            name, value = parse_param(line)
+            tuning_dict[name] = value
+    return tuning_dict
+
+def write_new_file(inputpath, outputpath):
+    pass
+
+if len(sys.argv) != 3:
     print('Missing arguments')
-    print('genevalparams.py <input>')
+    print('genevalparams.py <input> <output>')
     sys.exit(0)
 
-inputfile = sys.argv[1]
-outputfile = 'evalparams.c'
-output = open(outputfile, 'w')
+inputpath = sys.argv[1]
+outputpath = sys.argv[2]
 
-print_gpl_header(output)
-print_include_directives(output)
-output.write('\n')
+tuning_dict = read_input_file(inputpath)
 
-for line in open(inputfile):
-    line = line.rstrip()
-    if line[0] == '#':
-        continue
-    parts = line.split(' ')
-    if parts[0].startswith('psq_table_'):
-        print_psq_table(output, parts[0], line)
-    else:
-        print_param(output, parts[0], parts[1])
+tmpfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+
+with open(outputpath) as paramfile:
+    while True:
+        line = paramfile.readline()
+        if not line:
+            break
+        line = line.rstrip()
+        if len(line) == 0:
+            tmpfile.write('\n');
+            continue
+        if line.find('int') != 0:
+            tmpfile.write(f'{line}\n')
+            continue
+        name, value = parse_variable(paramfile, line)
+
+        tuning_name = name
+        if '[' in tuning_name:
+            tuning_name = tuning_name[:tuning_name.find('[')]
+        if tuning_name.lower() in tuning_dict:
+            value = tuning_dict[tuning_name.lower()]
+
+        write_variable(tmpfile, name, value)
+
+tmpname = tmpfile.name
+tmpfile.close()
+os.remove(outputpath)
+os.rename(tmpname, outputpath)
